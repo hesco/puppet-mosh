6c69f5d2fd82efdc6f8ea1f01b6b47210ab544bf
Hugh Esco <hesco@campaignfoundations.com>
2014-09-20 14:03:26 -0400

    implement use of puppetlabs-firewall for mosh

--- manifests/iptables.pp
+++ manifests/iptables.pp
@@ -2,10 +2,19 @@
 class mosh::iptables {
 
   $port_range = '60000:60005'
+  $port_range_puppet_firewall = '60000-60005'
   if defined('firewall') {
-    notify { 'need-to-implement':
-      message => 'Still need to implement use of puppetlabs-firewall for mosh',
+
+    firewall { '100 allow mosh connections':
+       dport => $port_range_puppet_firewall,
+       proto => udp,
+      action => accept,
     }
+
+    # notify { 'need-to-implement':
+    #   message => 'Still need to implement use of puppetlabs-firewall for mosh',
+    # }
+
   } else {
     exec { 'iptables-accept-mosh-udp':
       command => "/sbin/iptables -A INPUT -p udp -m multiport --dports $port_range -j ACCEPT ",
