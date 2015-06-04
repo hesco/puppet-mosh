2e8c92d74b74f67d410e747b2eafface5d053385
Dan Foster <dan@zem.org.uk>
2015-06-04 22:31:59 +0100

    Conditionally include the epel module to prevent duplicate declatations

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,14 +9,19 @@ class mosh {
     }
   
     'redhat': {
-       class { 'epel': }
-       # to get/enable EPEL repo
-       # puppet module install stahnma-epel / zerlgi-epel
-       package { $package:
-         ensure => present,
-         require => Class['epel'],
-        }
-     }
+      # It's likely that another module already includes the epel module.
+      # We only include it if it's not already defined to prevent 
+      # duplicate declarations.
+      if !defined(Class['::epel']) {
+        class { 'epel': }
+      }
+      # to get/enable EPEL repo
+      # puppet module install stahnma-epel / zerlgi-epel
+      package { $package:
+        ensure => present,
+        require => Class['epel'],
+      }
+    }
   }
 
   include mosh::iptables
